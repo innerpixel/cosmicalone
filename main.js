a117ebb811eeac222e45e91bb76089ce59acfc33
innerpixel <nsbasicus@yahoo.com>
2024-02-23 15:02:55 +0200

    local connection to stun server added / testing

--- main.js
+++ main.js
@@ -22,7 +22,8 @@ let peerConnection;
 const servers = {
     iceServers:[
         {
-            urls:['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
+            urls:['turn:185.247.117.93:3478', 'stun:cosmical.one:3478'], 
+
         }
     ]
 }
@@ -33,7 +34,7 @@ let constraints = {
         width:{min:640, ideal:1920, max:1920},
         height:{min:480, ideal:1080, max:1080},
     },
-    audio:true
+    audio:false
 }
 
 let init = async () => {
